68774cc9754f8b8f44efd49dccfa6ef1a1a578ab
AyanoNakashima <ayano.nakashima@lifebox.co.jp>
2023-03-14 20:49:05 +0900

    up

---
+++ src/main/java/com/example/Sample_2/Sample2_20.java
@@ -0,0 +1,28 @@
+package com.example.Sample_2;
+
+public class Sample2_20 {
+    class A {
+        void methodA(){
+            System.out.println("methodA()");
+        }
+    }
+
+    static class B{
+        static void methodB(){
+            System.out.println("methodB()");
+        }
+    }
+
+    public  static void main(String[] args){
+        new Sample2_20().new A().methodA();
+        // new A.methodA(); コンパイルエラー
+
+
+        // new Sample2_20().B().methodB();
+        new Sample2_20.B().methodB();
+
+        Sample2_20.B.methodB();
+        B.methodB();
+    }
+    
+}
